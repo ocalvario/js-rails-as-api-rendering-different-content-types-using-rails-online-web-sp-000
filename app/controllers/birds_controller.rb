835fe71b2b7e034cb872125adfbac68d04b7e4f9
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-11 20:10:27 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
-    class BirdsController < ApplicationController
-      def index
-        birds = Bird.all
-        render json: birds
-      end
-    end
+class BirdsController < ApplicationController
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+end
